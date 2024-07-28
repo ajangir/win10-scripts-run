18ef153aa04493e413640a43bbc89104debc98f8
Ajay Jangir <ajayjangir5252@gmail.com>
2024-07-28 21:33:48 +0530

    repo win10 backup: 2024-07-28 21:33

--- rename-subs-in-folder.ps1
+++ rename-subs-in-folder.ps1
@@ -1,1 +1,4 @@
 ls -directory | forEach-Object {echo $_.FullName; cp "$_\2_English.srt" "$_.srt" -verbose}
+adb shell ls -A sdcard/Download/0fdm/*.zip | ForEach-Object{adb pull "$_"; adb shell rm "$_"}
+ls -directory | forEach-Object {echo $_.FullName; $sub=ls $_|sort length -desc|select -first 1;cp $sub "$_.srt" -verbose}; mv *.srt ..
+ls *.zip | ForEach-Object{7z x $_}
